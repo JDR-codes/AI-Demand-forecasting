b2b05f77da838b1355951bd9b59cde3b1c937558
JDR-codes <deepakrajanjayaraman@gmail.com>
2026-06-12 11:11:36 +0530

    commit

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi_app.api.version1.routes.auth_router import api_router
+from fastapi_app.routes.auth_router import api_router
 from fastapi_app.db.session import init_db
 # Import models so they are registered with SQLAlchemy before init_db()
 from fastapi_app.models.auth_model import User  # noqa: F401
